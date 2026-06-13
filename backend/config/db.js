aaf10a9870758da235e93293bcdc35bff697df1b
GeorgioJabbour <georgiojabbour.g.gj@gmail.com>
2026-06-13 10:00:29 +0300

    Create db connection file

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+// ! .config reads your .env file parses its content and assign them to process.env so you can use them in other files
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.send.DB_DATABASE,
+});
+
+export default pool;
