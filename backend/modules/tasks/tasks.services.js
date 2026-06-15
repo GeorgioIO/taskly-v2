7663dd45da5ed93b509de9b666012a919ac1b446
GeorgioJabbour <georgiojabbour.g.gj@gmail.com>
2026-06-15 12:02:19 +0300

    Work on update task service function

--- backend/modules/tasks/tasks.services.js
+++ backend/modules/tasks/tasks.services.js
@@ -1,7 +1,50 @@
 import pool from "../../config/db.js";
 
-export async function getAll() {}
+export async function update(data) {
+  const { taskId, title, description, status, priority, dueDate } = data;
 
-export async function update() {}
+  const fields = [];
+  const params = [];
+  let paramsCounter = 1;
+
+  if (title) {
+    fields.push(`title = $${paramsCounter}`);
+    params.push(title);
+    paramsCounter++;
+  }
+
+  if (description) {
+    fields.push(`description = $${paramsCounter}`);
+    params.push(description);
+    paramsCounter++;
+  }
+
+  if (status) {
+    fields.push(`status = $${paramsCounter}`);
+    params.push(status);
+    paramsCounter++;
+  }
+
+  if (priority) {
+    fields.push(`priority = $${paramsCounter}`);
+    params.push(priority);
+    paramsCounter++;
+  }
+
+  if (dueDate) {
+    fields.push(`due_date = $${paramsCounter}`);
+    params.push(dueDate);
+    paramsCounter++;
+  }
+
+  const conditions = fields.join(",");
+  params.push(taskId);
+
+  const query = `UPDATE tasks SET ${conditions} WHERE id = $${paramsCounter} RETURNING id`;
+
+  const result = await pool.query(query, params);
+
+  return result;
+}
 
 export async function remove() {}
