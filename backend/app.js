e7b87ecfd0275cad3c6ea115a574f1595e385258
GeorgioJabbour <georgiojabbour.g.gj@gmail.com>
2026-06-16 17:59:02 +0300

    feat/added-no-route-found-middleware

--- backend/app.js
+++ backend/app.js
@@ -10,9 +10,13 @@ import pool from "./config/db.js";
 import { router as authRoutes } from "./modules/auth/auth.routes.js";
 import { router as userRoutes } from "./modules/users/users.routes.js";
 import { router as projectRoutes } from "./modules/projects/projects.routes.js";
+
+// Middlewares
 import errorHandler from "./middlewares/errorHandler.js";
 import generalLimiter from "./middlewares/generalLimiter.js";
 import authenticationLimiter from "./middlewares/authenticationLimiter.js";
+
+import { AppError } from "./utils/AppError.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -25,6 +29,8 @@ app.use(generalLimiter);
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/projects", projectRoutes);
+
+app.use((req, res, next) => next(new AppError("No route found", 404)));
 
 app.use(errorHandler);
 
